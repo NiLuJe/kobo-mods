9ab4da36e7657be9329caabc3ac46cd3fd6fb36d
NiLuJe <ninuje@gmail.com>
2019-09-19 22:36:47 +0200

    Stash the paths in variables

--- kobo-dotfile-hack/scripts/dotfile-patch.sh
+++ kobo-dotfile-hack/scripts/dotfile-patch.sh
@@ -7,8 +7,13 @@ if [ -e /mnt/onboard/.kobo/Kobo.tgz ] || [ -e /mnt/onboard/.kobo/KoboRoot.tgz ] 
 	exit 0
 fi
 
+# Constants
+KOBO_RCS="/etc/init.d/rcS"
+DBKP_RCS="/usr/local/geek1011/etc/dotfile-rcS.bak"
+
+
 # If we have a leftover backup, it means the patching failed, so don't try again.
-if [ -f /usr/local/geek1011/etc/dotfile-rcS.bak ] ; then
+if [ -f "${DBKP_RCS}" ] ; then
 	fbink -qmph -y 5 "[.H] Leftover backup, abort!"
 	exit 0
 fi
@@ -18,10 +23,10 @@ PATCH_DONE="false"
 
 # Make a backup...
 mkdir -p /usr/local/geek1011/etc
-cp -a /etc/init.d/rcS /usr/local/geek1011/etc/dotfile-rcS.bak
+cp -a "${KOBO_RCS}" "${DBKP_RCS}"
 
 # Do we need to patch?
-if ! grep -q dotfile-hack-pre-start /etc/init.d/rcS ; then
+if ! grep -q dotfile-hack-pre-start "${KOBO_RCS}" ; then
 	# Assume we'll have done something untoward to rcS!
 	PATCH_DONE="true"
 
@@ -29,12 +34,12 @@ if ! grep -q dotfile-hack-pre-start /etc/init.d/rcS ; then
 	PRE_PATT='# dotfile-hack-pre-start\nif [ -f /usr/local/geek1011/lib/libhidedir_kobo.so ] \&\& [ ! -f /etc/ld.so.preload ] ; then\n\texport LD_PRELOAD=/usr/local/geek1011/lib/libhidedir_kobo.so\nfi\n# dotfile-hack-pre-end'
 
 	# Do eeeet!
-	sed "/\/usr\/local\/Kobo\/nickel/i #%PRE_PATT%" -i /etc/init.d/rcS
-	sed -e "s:#%PRE_PATT%:${PRE_PATT}:" -i /etc/init.d/rcS
+	sed "/\/usr\/local\/Kobo\/nickel/i #%PRE_PATT%" -i "${KOBO_RCS}"
+	sed -e "s:#%PRE_PATT%:${PRE_PATT}:" -i "${KOBO_RCS}"
 fi
 
 # Do we need to patch?
-if ! grep -q dotfile-hack-post-start /etc/init.d/rcS ; then
+if ! grep -q dotfile-hack-post-start "${KOBO_RCS}" ; then
 	# Assume we'll have done something untoward to rcS!
 	PATCH_DONE="true"
 
@@ -42,8 +47,8 @@ if ! grep -q dotfile-hack-post-start /etc/init.d/rcS ; then
 	POST_PATT='# dotfile-hack-post-start\nunset LD_PRELOAD\n# dotfile-hack-post-end'
 
 	# Do eeeet!
-	sed "/\/usr\/local\/Kobo\/nickel/a #%POST_PATT%" -i /etc/init.d/rcS
-	sed -e "s:#%POST_PATT%:${POST_PATT}:" -i /etc/init.d/rcS
+	sed "/\/usr\/local\/Kobo\/nickel/a #%POST_PATT%" -i "${KOBO_RCS}"
+	sed -e "s:#%POST_PATT%:${POST_PATT}:" -i "${KOBO_RCS}"
 fi
 
 # If we applied a patch, reboot right now, as we modified the amount of lines in rcS, and shit may otherwise horribly blow up.
@@ -51,7 +56,7 @@ if [ "${PATCH_DONE}" = "true" ] ; then
 	sync
 
 	# Double-check that it was successful...
-	if grep -q dotfile-hack-pre-start /etc/init.d/rcS && grep -q dotfile-hack-pre-end /etc/init.d/rcS && grep -q dotfile-hack-post-start /etc/init.d/rcS && grep -q dotfile-hack-post-end /etc/init.d/rcS ; then
+	if grep -q dotfile-hack-pre-start "${KOBO_RCS}" && grep -q dotfile-hack-pre-end "${KOBO_RCS}" && grep -q dotfile-hack-post-start "${KOBO_RCS}" && grep -q dotfile-hack-post-end "${KOBO_RCS}" ; then
 		# Whee!
 		fbink -qmph -y 5 "[.H] Patching successful, rebooting . . ."
 
@@ -61,27 +66,26 @@ if [ "${PATCH_DONE}" = "true" ] ; then
 		fi
 
 		# Remove our backup
-		rm /usr/local/geek1011/etc/dotfile-rcS.bak
+		rm "${DBKP_RCS}"
 		sync
 
-		# And reboot (FIXME!)
-		echo reboot
+		# And reboot
+		reboot
 	else
 		# Uh oh...
 		fbink -qmph -y 5 "[.H] Patching failed, aborting . . ."
 
 		# Restore backup
-		cp -a /usr/local/geek1011/etc/dotfile-rcS.bak /etc/init.d/rcS
+		cp -a "${DBKP_RCS}" "${KOBO_RCS}"
 		# Keep the backup in place, so we don't try the patching again!
 		sync
 
-		# And reboot (FIXME!)
-		echo reboot
+		# And reboot
+		reboot
 	fi
 else
 	# Already patched, remove backup ;).
-	rm /usr/local/geek1011/etc/dotfile-rcS.bak
-	sync
+	rm "${DBKP_RCS}"
 fi
 
 exit 0
