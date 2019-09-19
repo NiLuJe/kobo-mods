6ab78791920a26b55c8fe6ef69e54ae92b834113
NiLuJe <ninuje@gmail.com>
2019-09-19 19:37:57 +0200

    Limit logging to successfully hidden directories

--- kobo-dotfile-hack/hidedir.c
+++ kobo-dotfile-hack/hidedir.c
@@ -64,43 +64,41 @@ constructor static void init() {
 
 static bool should_hide(DIR *dir, const char *name, const unsigned char type);
 
-#define WRAP_READDIR(dirent, readdir)                 \
-struct dirent *readdir(DIR *dir) {                    \
-    if (!wrap) return readdir##_orig(dir);            \
-    struct dirent *res;                               \
-    int err;                                          \
-    for (;;) {                                        \
-        res = readdir##_orig(dir);                    \
-        err = errno;                                  \
-        syslog(LOG_DEBUG, "%s: ", #readdir);  \
-        if (!res || !should_hide(dir, res->d_name, res->d_type)) { \
-            errno = err;                              \
-            return res;                               \
-        }                                             \
-    }                                                 \
-    errno = 0;                                        \
-    return NULL;                                      \
+#define WRAP_READDIR(dirent, readdir)                               \
+struct dirent *readdir(DIR *dir) {                                  \
+    if (!wrap) return readdir##_orig(dir);                          \
+    struct dirent *res;                                             \
+    int err;                                                        \
+    for (;;) {                                                      \
+        res = readdir##_orig(dir);                                  \
+        err = errno;                                                \
+        if (!res || !should_hide(dir, res->d_name, res->d_type)) {  \
+            errno = err;                                            \
+            return res;                                             \
+        }                                                           \
+    }                                                               \
+    errno = 0;                                                      \
+    return NULL;                                                    \
 }
 
 WRAP_READDIR(dirent,   readdir)
 WRAP_READDIR(dirent64, readdir64)
 
-#define WRAP_READDIR_R(dirent, readdir_r)                               \
-int readdir_r(DIR *dir, struct dirent *ent, struct dirent **res) {      \
-    if (!wrap) return readdir_r##_orig(dir, ent, res);                  \
-    int ret, err;                                                       \
-    for (;;) {                                                          \
-        ret = readdir_r##_orig(dir, ent, res);                          \
-        err = errno;                                                    \
-        syslog(LOG_DEBUG, "%s: ", #readdir_r);                            \
-        if (ret || !*res || !should_hide(dir, (*res)->d_name, (*res)->d_type)) {        \
-            errno = err;                                                \
-            return ret;                                                 \
-        }                                                               \
-    }                                                                   \
-    *res = NULL;                                                        \
-    errno = 0;                                                          \
-    return 0;                                                           \
+#define WRAP_READDIR_R(dirent, readdir_r)                                         \
+int readdir_r(DIR *dir, struct dirent *ent, struct dirent **res) {                \
+    if (!wrap) return readdir_r##_orig(dir, ent, res);                            \
+    int ret, err;                                                                 \
+    for (;;) {                                                                    \
+        ret = readdir_r##_orig(dir, ent, res);                                    \
+        err = errno;                                                              \
+        if (ret || !*res || !should_hide(dir, (*res)->d_name, (*res)->d_type)) {  \
+            errno = err;                                                          \
+            return ret;                                                           \
+        }                                                                         \
+    }                                                                             \
+    *res = NULL;                                                                  \
+    errno = 0;                                                                    \
+    return 0;                                                                     \
 }
 
 WRAP_READDIR_R(dirent,   readdir_r)
@@ -109,28 +107,22 @@ WRAP_READDIR_R(dirent64, readdir64_r)
 #ifndef USE_FULL_PATH
 
 static bool should_hide(DIR *dir __attribute__((unused)), const char *name, const unsigned char type) {
-    syslog(LOG_DEBUG, "should_hide\t\t\t\t`%s`\t?\t", name);
     if (type != DT_DIR) { // show anything that's not a directory
-        syslog(LOG_DEBUG, "NO (0)\n");
         return false;
     }
     if (strcmp(name, ".") == 0 || strcmp(name, "..") == 0) { // show **/. **/..
-        syslog(LOG_DEBUG, "NO (1)\n");
         return false;
     }
     if (strncasecmp(name, ".kobo", 5) == 0) { // show **/.kobo*
-        syslog(LOG_DEBUG, "NO (3)\n");
         return false;
     }
     if (strcasecmp(name, ".adobe-digital-editions") == 0) { // show **/.adobe-digital-editions
-        syslog(LOG_DEBUG, "NO (4)\n");
         return false;
     }
-    if (name[0] == '.') { // hide **/.* (but not everything underneath)
-        syslog(LOG_DEBUG, "YES (5)\n");
+    if (name[0] == '.') { // hide **/.* (preventing it from being traversed)
+        syslog(LOG_DEBUG, "Hid `%s` from nickel!\n", name);
         return true;
     } else {
-        syslog(LOG_DEBUG, "NO (5)\n");
         return false;
     }
 }
@@ -142,33 +134,26 @@ static bool should_hide(DIR *dir, const char *name, const unsigned char type) {
     char *path = "";
     if ((fd = dirfd(dir)) > 0 && dirpaths[fd]) {
         path = dirpaths[fd];
-        syslog(LOG_DEBUG, "should_hide\t\t\t\t`%s`\t\ton\t\t`%s`\t?\t", name, path);
     }
     if (type != DT_DIR) { // show anything that's not a directory
-        syslog(LOG_DEBUG, "NO (0)\n");
         return false;
     }
     if (strcmp(name, ".") == 0 || strcmp(name, "..") == 0) { // show **/. **/..
-        syslog(LOG_DEBUG, "NO (1)\n");
         return false;
     }
     if (strncasecmp(path, "/mnt/", 5) != 0) { // show !/mnt/**
-        syslog(LOG_DEBUG, "NO (2)\n");
         return false;
     }
     if (strncasecmp(name, ".kobo", 5) == 0 || strcasestr(path, "/.kobo")) { // show **/.kobo*/**
-        syslog(LOG_DEBUG, "NO (3)\n");
         return false;
     }
     if (strcasecmp(name, ".adobe-digital-editions") == 0 || strcasestr(path, "/.adobe-digital-editions")) { // show **/.adobe-digital-editions/**
-        syslog(LOG_DEBUG, "NO (4)\n");
         return false;
     }
     if (name[0] == '.') { // hide **/.* (preventing it from being traversed)
-        syslog(LOG_DEBUG, "YES (5)\n");
+        syslog(LOG_DEBUG, "Hid `%s/%s` from nickel!\n", path, name);
         return true;
     } else {
-        syslog(LOG_DEBUG, "NO (5)\n");
         return false;
     }
 }
@@ -179,12 +164,8 @@ DIR *opendir(const char *name) {
     DIR *dir = opendir_orig(name);
     err = errno;
     if (dir && (fd = dirfd(dir)) > 0) {
-        syslog(LOG_DEBUG, "opendir: `%s`\n", name);
         if (!(dirpaths[fd] = realpath(name, NULL))) {
             dirpaths[fd] = strdup(name);
-            syslog(LOG_DEBUG, "cached %d -> `%s` (as-is)\n", fd, name);
-        } else {
-            syslog(LOG_DEBUG, "cached %d -> `%s` (canonicalized)\n", fd, name);
         }
     }
     errno = err;
@@ -205,8 +186,6 @@ int closedir(DIR* dir) {
     if (!wrap) return closedir_orig(dir);
     int fd;
     if ((fd = dirfd(dir)) > 0 && dirpaths[fd]) {
-        syslog(LOG_DEBUG, "closedir: `%s`\n", dirpaths[fd]);
-        syslog(LOG_DEBUG, "clearing %d -> `%s`\n", fd, dirpaths[fd]);
         free(dirpaths[fd]);
         dirpaths[fd] = NULL;
     }
